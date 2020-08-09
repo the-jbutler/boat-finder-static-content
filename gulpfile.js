812f2425ded0120601aa38a7798911bda3784f47
Jordan Butler <jordanw2004@gmail.com>
2020-08-09 19:34:00 +0100

    Initial commit for static content
    Includes NPM, Gulp and SASS to handle the compilation of CSS
    so I can customise Bootstrap properly.
    
    Currently completed pages are index.html and about-us.html,
    placeholder images courtesy of placecage.com

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+function compileSass() {
+	return gulp.src('./sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
+}
+
+function watchSass() {
+	gulp.watch('./sass/**/*.scss', compileSass);
+}
+
+gulp.task('start', gulp.series(compileSass, watchSass));
+
+gulp.task('build', gulp.series(compileSass));
